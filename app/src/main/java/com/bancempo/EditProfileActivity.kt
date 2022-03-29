ee83b55f108b555c7434532bc36258f47a47a256
Carlos Rosero <carlos.asanza@student.kit.edu>
2022-03-29 11:07:44 +0200

    Modify activities to communicate via intents

--- app/src/main/java/com/bancempo/EditProfileActivity.kt
+++ app/src/main/java/com/bancempo/EditProfileActivity.kt
@@ -1,18 +1,31 @@
 package com.bancempo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.ContextMenu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
 import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        //val photo = findViewById<ImageView>(R.id.profile_pic).setImageURI(intent.getStringExtra("com.bancempo.PHOTO"))
+        val fullName = findViewById<TextView>(R.id.editTextFullName).setText(intent.getStringExtra("com.bancempo.FULL_NAME"))
+        val nickname = findViewById<TextView>(R.id.editTextNickname).setText(intent.getStringExtra("com.bancempo.NICKNAME"))
+        val email = findViewById<TextView>(R.id.editTextEmail).setText(intent.getStringExtra("com.bancempo.EMAIL"))
+        val location = findViewById<TextView>(R.id.editTextLocation).setText(intent.getStringExtra("com.bancempo.LOCATION"))
+        val skills = findViewById<TextView>(R.id.editTextSkills).setText(intent.getStringExtra("com.bancempo.SKILLS"))
+        val description = findViewById<TextView>(R.id.editTextDescription).setText(intent.getStringExtra("com.bancempo.DESCRIPTION"))
 
         val editPicture = findViewById<ImageButton>(R.id.changeImageButton)
         registerForContextMenu(editPicture)
@@ -38,5 +51,19 @@ class EditProfileActivity : AppCompatActivity() {
             }
             else -> super.onContextItemSelected(item)
         }
+    }
+
+    override fun onBackPressed() {
+        val i = Intent(this, ShowProfileActivity::class.java)
+        //i.putExtra("com.bancempo.PHOTO", findViewById<ImageView>(R.id.profile_pic).tag.toString())
+        i.putExtra("com.bancempo.FULL_NAME", findViewById<TextView>(R.id.editTextFullName).text.toString())
+        i.putExtra("com.bancempo.NICKNAME", findViewById<TextView>(R.id.editTextNickname).text.toString())
+        i.putExtra("com.bancempo.EMAIL", findViewById<TextView>(R.id.editTextEmail).text.toString())
+        i.putExtra("com.bancempo.LOCATION", findViewById<TextView>(R.id.editTextLocation).text.toString())
+        i.putExtra("com.bancempo.SKILLS", findViewById<TextView>(R.id.editTextSkills).text.toString())
+        i.putExtra("com.bancempo.DESCRIPTION", findViewById<TextView>(R.id.editTextDescription).text.toString())
+        setResult(Activity.RESULT_OK, i)
+
+        super.onBackPressed()
     }
 }
--- app/src/main/java/com/bancempo/ShowProfileActivity.kt
+++ app/src/main/java/com/bancempo/ShowProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.bancempo
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -48,10 +49,39 @@ class ShowProfileActivity : AppCompatActivity() {
         val skills = findViewById<TextView>(R.id.textViewSkills)
         val description = findViewById<TextView>(R.id.textViewDescription)
 
-        val i = Intent(this, EditProfileActivity::class.java).apply {
-            putExtra("com.bancempo.FULLNAME", fullName.text.toString())
-        }
+        val i = Intent(this, EditProfileActivity::class.java)
+        //i.putExtra("com.bancempo.PHOTO", photo.tag.toString())
+        i.putExtra("com.bancempo.FULL_NAME", fullName.text.toString())
+        i.putExtra("com.bancempo.NICKNAME", nickname.text.toString())
+        i.putExtra("com.bancempo.EMAIL", email.text.toString())
+        i.putExtra("com.bancempo.LOCATION", location.text.toString())
+        i.putExtra("com.bancempo.SKILLS", skills.text.toString())
+        i.putExtra("com.bancempo.DESCRIPTION", description.text.toString())
+
         startActivityForResult(i, 0)
         //startActivity(i)
+
+        //newString= extras.getString(“STRING_I_NEED”);
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            //val photo = findViewById<ImageView>(R.id.profile_pic)
+            //val fullName =
+            findViewById<TextView>(R.id.textViewFullName).setText(data.getStringExtra("com.bancempo.FULL_NAME"))
+            //val nickname =
+            findViewById<TextView>(R.id.textViewNickname).setText(data.getStringExtra("com.bancempo.NICKNAME"))
+            //val email =
+            findViewById<TextView>(R.id.textViewEmail).setText(data.getStringExtra("com.bancempo.EMAIL"))
+            //val location =
+            findViewById<TextView>(R.id.textViewLocation).setText(data.getStringExtra("com.bancempo.LOCATION"))
+            //val skills =
+            findViewById<TextView>(R.id.textViewSkills).setText(data.getStringExtra("com.bancempo.SKILLS"))
+            //val description =
+            findViewById<TextView>(R.id.textViewDescription).setText(data.getStringExtra("com.bancempo.DESCRIPTION"))
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
