f14277c81b0b803f62af0dfa23ecb69423a2cc71
patriziodegirolamo <patrizio.degirolamo@gmail.com>
2022-04-22 18:49:12 +0200

    BOTTONE + FUNZIONANTE

--- app/src/main/java/com/bancempo/SmallAdvertisment.kt
+++ app/src/main/java/com/bancempo/SmallAdvertisment.kt
@@ -5,14 +5,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 data class SmallAdv(val title:String, val date:String) {
 }
 
-class SmallAdvAdapter(val data: List<SmallAdv>) : RecyclerView.Adapter<SmallAdvAdapter.SmallAdvHolder>(){
+class SmallAdvAdapter(val data: MutableList<SmallAdv>) : RecyclerView.Adapter<SmallAdvAdapter.SmallAdvHolder>(){
     class SmallAdvHolder(v:View) : RecyclerView.ViewHolder(v){
         val title: TextView = v.findViewById(R.id.tvSmallAdvTitle)
         val date: TextView = v.findViewById(R.id.tvsmallAdvDate)
+        //val modify: ImageView = v.find...
+
+
+        fun bind(adv: SmallAdv, action: (v:View)-> Unit){
+            title.text = adv.title
+            date.text = adv.date
+            //modify.setOnClickListener(action)
+        }
+
+        fun unbind(){
+            println("cosa succede su UNbind")
+            //modify.setOnClickListener(null)
+        }
 
     }
 
@@ -25,9 +39,13 @@ class SmallAdvAdapter(val data: List<SmallAdv>) : RecyclerView.Adapter<SmallAdvA
         return SmallAdvHolder(vg)
     }
 
+    //ti dice quale elemento della lista è correntemente visibile e la sua posizione il lista
     override fun onBindViewHolder(holder: SmallAdvHolder, position: Int) {
-        holder.title.text = data[position].title
-        holder.date.text = data[position].date
+        holder.bind(data[position]){
+
+        }
+
+
     }
 
     override fun getItemCount(): Int = data.size
--- app/src/main/java/com/bancempo/TimeSlotListFragment.kt
+++ app/src/main/java/com/bancempo/TimeSlotListFragment.kt
@@ -8,14 +8,22 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TimeSlotListFragment : Fragment(R.layout.fragment_time_slot_list){
+    var newPos = 0
+    lateinit var adapter : SmallAdvAdapter
+    lateinit var llm : LinearLayoutManager
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val buttonProfile = view.findViewById<Button>(R.id.buttonProfile)
         val buttonList = view.findViewById<Button>(R.id.buttonList)
+        val fab = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        val rv = view.findViewById<RecyclerView>(R.id.recyclerView)
 
+        val advs:MutableList<SmallAdv> = createInitialListOfAdvs(15)
 
         buttonProfile.setOnClickListener{
             findNavController().navigate(R.id.action_timeSlotListFragment_to_showProfileFragment)
@@ -25,19 +33,40 @@ class TimeSlotListFragment : Fragment(R.layout.fragment_time_slot_list){
             findNavController().navigate(R.id.action_timeSlotListFragment_to_timeSlotDetailsFragment)
         }
 
-        val rv = view.findViewById<RecyclerView>(R.id.recyclerView)
-        rv.layoutManager = LinearLayoutManager(context)
-        val adapter = SmallAdvAdapter(createAdvs(15))
+        //se schiaccio il fab (a runtime!), setto llm e adapter
+        fab.setOnClickListener{
+            advs.add(newPos, SmallAdv("adv${newPos}", "${newPos}/01/12"))
+            newPos++
+
+            //ricarico adapter e llm
+            adapter = SmallAdvAdapter(advs)
+            llm = LinearLayoutManager(context)
+
+            //setto adapter e llm
+            llm.stackFromEnd = true
+            adapter.notifyItemInserted(advs.size)
+
+            //setto la view
+            rv.layoutManager = llm
+            rv.adapter = adapter
+
+        }
+
+        //di default setto llm e adapter
+        llm = LinearLayoutManager(context)
+        adapter = SmallAdvAdapter(advs)
+        rv.layoutManager = llm
         rv.adapter = adapter
     }
 
-    private fun createAdvs(n: Int): List<SmallAdv>{
+    private fun createInitialListOfAdvs(n: Int): MutableList<SmallAdv>{
         val l = mutableListOf<SmallAdv>()
-        val end = n%28
-        for(i in 1..end){
+        newPos = n
+        for(i in 0..n-1){
             val k = SmallAdv("adv${i}", "${i}/01/12")
             l.add(k)
         }
         return l
     }
+
 }
