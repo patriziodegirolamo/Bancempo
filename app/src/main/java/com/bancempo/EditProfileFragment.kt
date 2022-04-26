043e6edef5432737c1ca613a47d24105bda055a3
Carlos Rosero <carlos@rosero.io>
2022-04-26 16:09:11 +0200

    WIP convert ShowProfile and EditProfile to Fragments

--- app/src/main/java/com/bancempo/EditProfileFragment.kt
+++ app/src/main/java/com/bancempo/EditProfileFragment.kt
@@ -1,17 +1,365 @@
 package com.bancempo
+/*
 
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.ContextWrapper
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
+import android.view.MenuItem
 import android.view.View
-import android.widget.TextView
+import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import java.io.*
 
 class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
+
+    lateinit var fullName : TextView
+    lateinit var photo : ImageView
+    lateinit var nickname : TextView
+    lateinit var email : TextView
+    lateinit var location : TextView
+    lateinit var skills : TextView
+    lateinit var description : TextView
+    lateinit var editPicture : ImageButton
+
+    lateinit var editText: EditText
+    lateinit var addchipbutton: ImageButton
+    lateinit var chipGroup: ChipGroup
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    val SELECT_PICTURE = 200
+
+    var w: Int = 0
+    var h: Int = 0
+
+    //var uri_or_bitmap:String = ""
+    var bitmap_photo : Bitmap? = null
+    var uri_photo : Uri? = null
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val tv = view.findViewById<TextView>(R.id.textView)
         tv.setOnClickListener{
             findNavController().navigate(R.id.action_editProfileFragment_to_showProfileFragment)
         }
+
+        photo = view.findViewById<ImageView>(R.id.profile_pic)
+        editPicture = view.findViewById<ImageButton>(R.id.changeImageButton)
+        fullName = view.findViewById<TextView>(R.id.editTextFullName)
+        nickname = view.findViewById<TextView>(R.id.editTextNickname)
+        email = view.findViewById<TextView>(R.id.editTextEmail)
+        location = view.findViewById<TextView>(R.id.editTextLocation)
+        skills = view.findViewById<TextView>(R.id.editTextSkills)
+        description = view.findViewById<TextView>(R.id.editTextDescription)
+
+        editText = view.findViewById<EditText>(R.id.editTextSkills)
+        addchipbutton = view.findViewById<ImageButton>(R.id.addchipbutton)
+        chipGroup = view.findViewById<ChipGroup>(R.id.chipGroup)
+
+        addchipbutton.setOnClickListener{
+            if(!editText.text.toString().isEmpty()){
+                addChip(editText.text.toString())
+
+                editText.setText("")
+            }
+        }
+
+        val orientation: Int = this.resources.configuration.orientation
+
+        /*
+        if (orientation === Configuration.ORIENTATION_LANDSCAPE ) {
+            v1.viewTreeObserver.addOnGlobalLayoutListener(object :
+                ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    h = v1.height
+                    w = v1.width
+                    Log.d("Layout", "v1.requestLayout(): $w,$h")
+                    //v5.post { v5.layoutParams = LinearLayout.LayoutParams(w / 3, h) }
+                    v1.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
+
+        } else {
+            sv.viewTreeObserver.addOnGlobalLayoutListener(object :
+                ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    h = sv.height
+                    w = sv.width
+                    Log.d("Layout", "v1.requestLayout(): $w,$h")
+                    v5.post { v5.layoutParams = LinearLayout.LayoutParams(w, h / 3) }
+                    sv.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
+        }
+
+         */
+
+
+        if (savedInstanceState != null) {
+            loadImageFromStorage("/data/user/0/com.bancempo/app_imageDir")
+            fullName.text = savedInstanceState.getString("full_name")
+            nickname.text = savedInstanceState.getString("nickname")
+            email.text = savedInstanceState.getString("email")
+            location.text = savedInstanceState.getString("location")
+            description.text = savedInstanceState.getString("description")
+
+            val skillsString = savedInstanceState.getString("skills")
+            if (skillsString != null) {
+                chipGroup.removeAllViews()
+                skillsString.split(",").forEach {
+                    var chip = Chip(activity)
+                    if(!it.isEmpty()) {
+                        chip.setText(it)
+                        chip.isCloseIconVisible = true
+
+                        chip.setOnCloseIconClickListener {
+                            chipGroup.removeView(chip)
+                        }
+                        chipGroup.addView(chip)
+                    }
+                }
+            }
+        }
+
+        else{
+            val bmp = decodeBase64(requireActivity().intent.getStringExtra("com.bancempo.PHOTO"))
+            photo.setImageBitmap(bmp)
+
+            fullName.text = requireActivity().intent.getStringExtra("com.bancempo.FULL_NAME")
+            nickname.text = requireActivity().intent.getStringExtra("com.bancempo.NICKNAME")
+            email.text = requireActivity().intent.getStringExtra("com.bancempo.EMAIL")
+            location.text = requireActivity().intent.getStringExtra("com.bancempo.LOCATION")
+            description.text = requireActivity().intent.getStringExtra("com.bancempo.DESCRIPTION")
+
+            val skillsString = requireActivity().intent.getStringExtra("com.bancempo.SKILLS")
+            chipGroup.removeAllViews()
+            if (skillsString != null && skillsString != "") {
+                skillsString.split(",").forEach {
+                    var chip = Chip(this)
+                    if(!it.isEmpty()) {
+                        chip.setText(it)
+                        chip.isCloseIconVisible = true
+
+                        chip.setOnCloseIconClickListener {
+                            chipGroup.removeView(chip)
+                        }
+                        chipGroup.addView(chip)
+                    }
+                }
+            }
+
+        }
+        editPicture.setOnClickListener {
+            showPopup(editPicture)
+        }
     }
+
+    @SuppressLint("SdCardPath")
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("full_name", fullName.text.toString())
+        outState.putString("nickname", nickname.text.toString())
+        outState.putString("email", email.text.toString())
+        outState.putString("location", location.text.toString())
+        outState.putString("description", description.text.toString())
+
+        var chipText = ""
+        for (i in 0 until chipGroup.childCount) {
+            val chip = chipGroup.getChildAt(i) as Chip
+            chipText += "${chip.text},"
+        }
+        outState.putString("skills", chipText)
+    }
+
+    private fun showPopup(v: View) {
+        val popup = PopupMenu(this, v)
+        popup.menuInflater.inflate(R.menu.menu_profile_picture, popup.menu)
+
+        popup.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item: MenuItem? ->
+            when (item!!.itemId) {
+                R.id.select_image_gallery -> {
+                    val i = Intent(Intent.ACTION_GET_CONTENT)
+                    i.type = "image/*"
+                    // pass the constant to compare it with the returned requestCode
+                    startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE)
+                    true
+                }
+                R.id.use_camera -> {
+                    dispatchTakePictureIntent()
+                    true
+                }
+            }
+            false
+        })
+
+        popup.show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    @SuppressLint("WrongThread")
+    override fun onBackPressed() {
+        val i = Intent(activity, ShowProfileActivity::class.java)
+
+        //ENCODE bitmap
+        if (bitmap_photo != null){
+            i.putExtra("com.bancempo.PHOTO", encodeToBase64(bitmap_photo!!))
+        }
+        else if (uri_photo != null){
+            i.putExtra("com.bancempo.PHOTO", uri_photo.toString())
+        }
+
+        i.putExtra("com.bancempo.FULL_NAME", requireView().findViewById<TextView>(R.id.editTextFullName).text.toString())
+        i.putExtra("com.bancempo.NICKNAME", requireView().findViewById<TextView>(R.id.editTextNickname).text.toString())
+        i.putExtra("com.bancempo.EMAIL", requireView().findViewById<TextView>(R.id.editTextEmail).text.toString())
+        i.putExtra("com.bancempo.LOCATION", requireView().findViewById<TextView>(R.id.editTextLocation).text.toString())
+        i.putExtra("com.bancempo.DESCRIPTION", requireView().findViewById<TextView>(R.id.editTextDescription).text.toString())
+
+        var chipText = ""
+        for (i in 0 until chipGroup.childCount) {
+            val chip = chipGroup.getChildAt(i) as Chip
+            chipText += "${chip.text},"
+        }
+
+        i.putExtra("com.bancempo.SKILLS", chipText)
+
+        setResult(Activity.RESULT_OK, i)
+        super.onBackPressed()
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+            Toast.makeText(applicationContext, R.string.camera_not_available, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == AppCompatActivity.RESULT_OK && data != null) {
+            bitmap_photo = data.extras?.get("data") as Bitmap
+            saveToInternalStorage(bitmap_photo!!) + "profile.jpeg"
+            view.findViewById<ImageView>(R.id.profile_pic).setImageBitmap(bitmap_photo)
+
+        } else if (requestCode == SELECT_PICTURE && resultCode == AppCompatActivity.RESULT_OK && data != null){
+            // Get the url of the image from data
+            uri_photo = data.data
+            // update the preview image in the layout
+
+            //uri_or_bitmap = "uri"
+            val bmp:Bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri_photo)
+
+            val ins: InputStream? = applicationContext.contentResolver.openInputStream(uri_photo!!)
+            val ei = ExifInterface(ins!!)
+
+            val or = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)
+            val rotatedBitmap: Bitmap = when (or) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(bmp, 90f)
+                ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(bmp, 180f)
+                ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(bmp, 270f)
+                ExifInterface.ORIENTATION_NORMAL -> bmp
+                else -> bmp
+            }
+
+            ins.close()
+            saveToInternalStorage(rotatedBitmap) + "profile.jpeg"
+
+            view.findViewById<ImageView>(R.id.profile_pic).setImageURI(uri_photo)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun addChip(text: String) {
+        val chip = Chip(this)
+        chip.text = text
+
+        chip.isCloseIconVisible = true
+
+        chip.setOnCloseIconClickListener{
+            chipGroup.removeView(chip)
+        }
+        chipGroup.addView(chip)
+    }
+
+
+    fun encodeToBase64(image: Bitmap): String? {
+        val baos = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val b = baos.toByteArray()
+        val imageEncoded: String = Base64.encodeToString(b, Base64.DEFAULT)
+        //Log.d("Image Log:", imageEncoded)
+        baos.close()
+        return imageEncoded
+    }
+
+    fun decodeBase64(input: String?): Bitmap? {
+        val decodedByte: ByteArray = Base64.decode(input, 0)
+        return BitmapFactory
+            .decodeByteArray(decodedByte, 0, decodedByte.size)
+    }
+
+
+    private fun saveToInternalStorage(bitmapImage: Bitmap): String? {
+        val cw = ContextWrapper(applicationContext)
+        // path to /data/data/yourapp/app_data/imageDir
+        val directory: File = cw.getDir("imageDir", AppCompatActivity.MODE_PRIVATE)
+        // Create imageDir
+        val mypath = File(directory, "profile.jpeg")
+        val fos: FileOutputStream?
+        try {
+            fos = FileOutputStream(mypath)
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+            fos.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return directory.getAbsolutePath()
+    }
+
+
+    fun rotateImage(source: Bitmap, angle: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(angle)
+
+        return Bitmap.createBitmap(
+            source, 0, 0, source.width, source.height,
+            matrix, true
+        )
+    }
+
+    private fun loadImageFromStorage(path: String) {
+        try {
+            val f = File(path, "profile.jpeg")
+            val fis = FileInputStream(f)
+            val b = BitmapFactory.decodeStream(fis)
+            val img = view.findViewById<ImageView>(R.id.profile_pic)
+            fis.close()
+            img.setImageBitmap(b)
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        }
+    }
+    
 }
+
+*/
+ */
--- app/src/main/java/com/bancempo/ShowProfileFragment.kt
+++ app/src/main/java/com/bancempo/ShowProfileFragment.kt
@@ -1,6 +1,235 @@
 package com.bancempo
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.util.Base64
+import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.core.view.drawToBitmap
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import org.json.JSONObject
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileNotFoundException
 
 class ShowProfileFragment : Fragment(R.layout.fragment_show_profile) {
+
+    lateinit var fullName : TextView
+    lateinit var photo : ImageView
+    lateinit var nickname : TextView
+    lateinit var email : TextView
+    lateinit var location : TextView
+    lateinit var skills : TextView
+    lateinit var description : TextView
+
+    lateinit var chipGroup : ChipGroup
+
+    var w: Int = 0
+    var h: Int = 0
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        fullName = view.findViewById<TextView>(R.id.textViewFullName)
+        photo = view.findViewById<ImageView>(R.id.profile_pic)
+        nickname = view.findViewById<TextView>(R.id.textViewNickname)
+        email = view.findViewById<TextView>(R.id.textViewEmail)
+        location = view.findViewById<TextView>(R.id.textViewLocation)
+        skills = view.findViewById<TextView>(R.id.textViewSkills)
+        description = view.findViewById<TextView>(R.id.textViewDescription)
+        chipGroup = view.findViewById<ChipGroup>(R.id.chipGroup)
+
+
+        val orientation: Int = this.resources.configuration.orientation
+
+
+
+        loadImageFromStorage("/data/user/0/com.bancempo/app_imageDir")
+
+        if (savedInstanceState != null) {
+
+
+            fullName.text = savedInstanceState.getString("full_name")
+            nickname.text = savedInstanceState.getString("nickname")
+            email.text = savedInstanceState.getString("email")
+            location.text = savedInstanceState.getString("location")
+            description.text = savedInstanceState.getString("description")
+
+            val skillsString = savedInstanceState.getString("skills")
+            chipGroup.removeAllViews()
+            if (skillsString != null && skillsString != "") {
+                skillsString.split(",").forEach {
+                    var chip = Chip(activity)
+                    if(!it.isEmpty()) {
+                        chip.setText(it)
+                        chipGroup.addView(chip)
+                    }
+                }
+            }
+        }
+
+        else{
+            val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+            val stringJSON:String? = sharedPref.getString("bancempoJSON", "")
+            var jObject: JSONObject? = null
+            if(stringJSON != null && stringJSON != "") {
+                jObject = JSONObject(stringJSON)
+                fullName.text = jObject.getString(getString(R.string.full_name))
+                nickname.text = jObject.getString(getString(R.string.nickname))
+                email.text = jObject.getString(getString(R.string.email))
+                location.text = jObject.getString(getString(R.string.location))
+                description.text = jObject.getString(getString(R.string.description))
+                val skillsString = jObject.getString(getString(R.string.skills))
+                chipGroup.removeAllViews()
+                if (skillsString != "") {
+                    skillsString.split(",").forEach {
+                        var chip = Chip(activity)
+                        if(!it.isEmpty()) {
+                            chip.setText(it)
+                            chipGroup.addView(chip)
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    @SuppressLint("SdCardPath")
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("full_name", fullName.text.toString())
+        outState.putString("nickname", nickname.text.toString())
+        outState.putString("email", email.text.toString())
+        outState.putString("location", location.text.toString())
+        outState.putString("description", description.text.toString())
+
+        var chipText = ""
+        for (i in 0 until chipGroup.childCount) {
+            val chip = chipGroup.getChildAt(i) as Chip
+            chipText += "${chip.text},"
+        }
+        outState.putString("skills", chipText)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_profile, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.edit_profile -> {
+                editProfile()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun editProfile() {
+        val i = Intent(activity, EditProfileActivity::class.java)
+        i.putExtra("com.bancempo.PHOTO", encodeToBase64(photo.drawToBitmap()))
+        i.putExtra("com.bancempo.FULL_NAME", fullName.text.toString())
+        i.putExtra("com.bancempo.NICKNAME", nickname.text.toString())
+        i.putExtra("com.bancempo.EMAIL", email.text.toString())
+        i.putExtra("com.bancempo.LOCATION", location.text.toString())
+        i.putExtra("com.bancempo.DESCRIPTION", description.text.toString())
+
+        var chipText = ""
+        for (i in 0 until chipGroup.childCount) {
+            val chip = chipGroup.getChildAt(i) as Chip
+            chipText += "${chip.text},"
+        }
+        i.putExtra("com.bancempo.SKILLS", chipText)
+
+        startActivityForResult(i, 0)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            loadImageFromStorage("/data/user/0/com.bancempo/app_imageDir")
+
+            fullName.text = data.getStringExtra("com.bancempo.FULL_NAME")
+            nickname.text = data.getStringExtra("com.bancempo.NICKNAME")
+            email.text = data.getStringExtra("com.bancempo.EMAIL")
+            location.text = data.getStringExtra("com.bancempo.LOCATION")
+            description.text = data.getStringExtra("com.bancempo.DESCRIPTION")
+
+            val skillsString = data.getStringExtra("com.bancempo.SKILLS")
+            chipGroup.removeAllViews()
+            if (skillsString != null && skillsString != "") {
+                skillsString.split(",").forEach {
+                    var chip = Chip(activity)
+                    if(!it.isEmpty()) {
+                        chip.setText(it)
+                        chipGroup.addView(chip)
+                    }
+                }
+            }
+
+            val jObject = JSONObject()
+            jObject.put(getString(R.string.full_name), fullName.text.toString())
+            jObject.put(getString(R.string.nickname), nickname.text.toString())
+            jObject.put(getString(R.string.email), email.text.toString())
+            jObject.put(getString(R.string.location), location.text.toString())
+            jObject.put(getString(R.string.description), description.text.toString())
+
+            var chipText = ""
+            for (i in 0 until chipGroup.childCount) {
+                val chip = chipGroup.getChildAt(i) as Chip
+                chipText += "${chip.text},"
+            }
+            jObject.put(getString(R.string.skills), chipText)
+
+            //save all the textviews in the shared_preferences file
+            val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+            with (sharedPref.edit()) {
+                putString("bancempoJSON", jObject.toString())
+                apply()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+
+
+    private fun loadImageFromStorage(path: String) {
+        try {
+            val f = File(path, "profile.jpeg")
+            val fis = FileInputStream(f)
+            val b = BitmapFactory.decodeStream(fis)
+            val img = requireView().findViewById<ImageView>(R.id.profile_pic)
+            fis.close()
+            img.setImageBitmap(b)
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun encodeToBase64(image: Bitmap): String? {
+        val baos = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val b = baos.toByteArray()
+        val imageEncoded: String = Base64.encodeToString(b, Base64.DEFAULT)
+        //Log.d("Image Log:", imageEncoded)
+        baos.close()
+        return imageEncoded
+    }
+
+
 }
