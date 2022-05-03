ba6cf56580fcc1192983d4d39b09b86f407206f8
patriziodegirolamo <patrizio.degirolamo@gmail.com>
2022-05-03 09:40:56 +0200

    dialog inserted

--- app/src/main/java/com/bancempo/TimeSlotEditFragment.kt
+++ app/src/main/java/com/bancempo/TimeSlotEditFragment.kt
@@ -1,33 +1,35 @@
 package com.bancempo
 
+import android.text.format.DateFormat
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
-import android.sax.TextElementListener
+import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.DatePicker
 import android.widget.TextView
 import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
-import com.google.android.material.textfield.TextInputEditText
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputLayout
 import java.util.*
 
 class TimeSlotEditFragment : Fragment(R.layout.fragment_time_slot_edit){
     private lateinit var title: TextInputLayout
     private lateinit var description: TextInputLayout
-    private lateinit var selectedDates: TextInputLayout
-    private lateinit var date: DatePicker
     private lateinit var location: TextInputLayout
-    private lateinit var time: TimePicker
     private lateinit var note: TextInputLayout
-
+    private lateinit var date: TextView
+    private lateinit var timeslot: TextView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val year: Int
-        val month: Int
-        val day: Int
-        val string_date: String?
 
         title = view.findViewById(R.id.edit_title)
         description = view.findViewById(R.id.edit_description)
@@ -39,20 +41,118 @@ class TimeSlotEditFragment : Fragment(R.layout.fragment_time_slot_edit){
         location.placeholderText = arguments?.getString("location")
         note.placeholderText = arguments?.getString("note")
 
-        /*
-        string_date = arguments?.getString("date");
 
-        if(string_date != null) {
-            year = string_date.split("/").elementAt(0).toInt()
-            month = string_date.split("/").elementAt(1).toInt()
-            day = string_date.split("/").elementAt(2).toInt()
+        date = view.findViewById(R.id.tvDate)
+        timeslot = view.findViewById(R.id.tvTime)
 
-            date.init(year,month,day, DatePicker.OnDateChangedListener { date, year, month, day->
-                selectedDates.placeholderText = "Year: "+ year + " Month: "+ (month+1) + " Day: "+day
-            })
+
+        val buttonDate = view.findViewById<Button>(R.id.buttonDate)
+        buttonDate.setOnClickListener { showDialogOfDatePicker() }
+
+        val buttonTime = view.findViewById<Button>(R.id.buttonTime)
+        buttonTime.setOnClickListener { showDialogOfTimeSlotPicker() }
+    }
+
+    //Date
+    private fun showDialogOfDatePicker() {
+        val datePickerFragment = DatePickerFragment(date)
+        datePickerFragment.show(requireActivity().supportFragmentManager, "datePicker")
+    }
+
+
+    class DatePickerFragment(private val date: TextView) : DialogFragment(),DatePickerDialog.OnDateSetListener {
+
+        private var c = Calendar.getInstance()
+        private var year = c.get(Calendar.YEAR)
+        private var month = c.get(Calendar.MONTH)
+        private var day = c.get(Calendar.DAY_OF_MONTH)
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            retainInstance = true
         }
 
-         */
+        override fun onSaveInstanceState(outState: Bundle) {
+            super.onSaveInstanceState(outState)
+            outState.putInt("year", year)
+            outState.putInt("month", month)
+            outState.putInt("day", day)
+        }
 
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            if(savedInstanceState!= null){
+                year = savedInstanceState.getInt("year")
+                month = savedInstanceState.getInt("month")
+                day = savedInstanceState.getInt("day")
+            }
+            else{
+                year = c.get(Calendar.YEAR)
+                month = c.get(Calendar.MONTH)
+                day = c.get(Calendar.DAY_OF_MONTH)
+            }
+            return DatePickerDialog(requireContext(), this, year, month, day)
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+            this.year = year
+            this.month = month
+            this.day = day
+            date.text = "${day}/${(month + 1)}/${year}"
+        }
+
+    }
+
+    //Time
+    private fun showDialogOfTimeSlotPicker() {
+        val timeslotPickerFragment = TimePickerFragment(timeslot)
+        timeslotPickerFragment.show(requireActivity().supportFragmentManager, "timePicker")
+    }
+
+    class TimePickerFragment(private val timeslot: TextView) : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+        private var c = Calendar.getInstance()
+        private var hour = c.get(Calendar.HOUR_OF_DAY)
+        private var minute = c.get(Calendar.MINUTE)
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            retainInstance = true
+        }
+
+        override fun onSaveInstanceState(outState: Bundle) {
+            outState.putInt("hour", hour)
+            outState.putInt("minute", minute)
+            super.onSaveInstanceState(outState)
+        }
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+            if(savedInstanceState!= null){
+                hour = savedInstanceState.getInt("hour")
+                minute = savedInstanceState.getInt("minute")
+            }
+            else{
+                hour = c.get(Calendar.HOUR)
+                minute = c.get(Calendar.MINUTE)
+            }
+
+            return TimePickerDialog(
+                activity,
+                this,
+                hour,
+                minute,
+                DateFormat.is24HourFormat(activity)
+            )
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+            if (minute in 0..9) {
+                timeslot.text = "$hourOfDay:0$minute"
+            } else {
+                timeslot.text = "$hourOfDay:$minute"
+            }
+        }
     }
 }
