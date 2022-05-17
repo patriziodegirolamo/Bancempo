b25b656a091ecef3c18dfa64c83f28ca08e30d0c
Carlos Rosero <carlos@rosero.io>
2022-05-17 21:49:53 +0200

    Implement first draft for login screen

--- app/src/main/java/com/bancempo/activities/MainActivity.kt
+++ app/src/main/java/com/bancempo/activities/MainActivity.kt
@@ -1,26 +1,24 @@
 package com.bancempo.activities
 
-import android.app.Activity
+import com.bancempo.R
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.LiveData
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
-import com.bancempo.R
-import com.firebase.ui.auth.IdpResponse
-import com.firebase.ui.auth.AuthUI
-import com.bancempo.models.SharedViewModel
-import com.bancempo.Skill
 import com.bancempo.data.User
 import com.bancempo.databinding.ActivityMainBinding
+import com.bancempo.models.SharedViewModel
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -108,13 +106,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun login() {
         val providers = arrayListOf(
-            AuthUI.IdpConfig.GoogleBuilder().build()
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build()
         )
+
+        val customLayout = AuthMethodPickerLayout.Builder(R.layout.sign_in)
+            .setGoogleButtonId(R.id.sign_in_button)
+            .setPhoneButtonId(R.id.sign_in_phone_button)
+            .build()
 
         // Create and launch sign-in intent
         startActivityForResult(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
+                .setAuthMethodPickerLayout(customLayout)
+                .setTheme(R.style.FirebaseUI_DefaultMaterialTheme)
                 .setAvailableProviders(providers)
                 .build(),
             RC_SIGN_IN
